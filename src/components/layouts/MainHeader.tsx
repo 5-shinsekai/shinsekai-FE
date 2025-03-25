8ec2c4a2ad5d218cce46042371640f6d98ed9627
unknown <corosica1@gmail.com>
2025-03-25 17:40:12 +0900

    Feat : 메인 헤더 navbar 기초 제작

--- src/components/layouts/MainHeader.tsx
+++ src/components/layouts/MainHeader.tsx
@@ -1,14 +1,21 @@
-import { navMenuData } from "@/data/InitialData";
+import { navLeftMenuData, navRightMenuData } from "@/data/InitialData";
 import { gnbMenuType } from "@/types/InitialDataTypes";
 import React from "react";
 
 export default function MainHeader() {
   return (
     <header>
-      <p>헤더입니다.</p>
-      <nav className="w-full px-[16px] py-3">
+      <nav className="w-full px-[16px] py-3 grid grid-cols-3 gap-4">
+      <ul className="w-full flex justify-between items-center">
+          {navLeftMenuData.map((menu: gnbMenuType) => (
+            <li key={menu.id}>
+              <menu.icon className="" />
+            </li>
+          ))}
+        </ul>
+        <p className=" text-center items-center">메인 페이지</p>
         <ul className="w-full flex justify-between items-center">
-          {navMenuData.map((menu: gnbMenuType) => (
+          {navRightMenuData.map((menu: gnbMenuType) => (
             <li key={menu.id}>
               <menu.icon className="" />
             </li>
--- src/data/InitialData.tsx
+++ src/data/InitialData.tsx
@@ -1,22 +1,33 @@
+import CartIcon from "@/components/ui/icons/CartIcon";
 import cartIcon from "@/components/ui/icons/CartIcon";
+import CloseIcon from "@/components/ui/icons/CloseIcon";
 import gnbMenuIcon from "@/components/ui/icons/GnbMenuIcon";
+import SearchIcon from "@/components/ui/icons/SearchIcon";
 import searchIcon from "@/components/ui/icons/SearchIcon";
 import { gnbMenuType } from "@/types/InitialDataTypes";
 
-export const navMenuData: gnbMenuType[] = [
+export const navLeftMenuData: gnbMenuType[] = [
   {
     id: 1,
     title: "gnb",
     icon: gnbMenuIcon,
+  }
+];
+
+export const navRightMenuData: gnbMenuType[] = [
+  {
+    id: 1,
+    title: "gnb",
+    icon: SearchIcon,
   },
   {
     id: 2,
     title: "search",
-    icon: searchIcon,
+    icon: CartIcon,
   },
   {
     id: 3,
     title: "cart",
-    icon: cartIcon,
+    icon: CloseIcon,
   },
 ];
