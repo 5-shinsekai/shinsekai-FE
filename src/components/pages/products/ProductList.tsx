259f0010c89415eae4a7ce8d8d7cb0169c9f3c7b
unknown <corosica1@gmail.com>
2025-03-27 17:29:31 +0900

    Feat : 이벤트 페이지 기본 틀 제작 - 이벤트(기획전) 기본 틀 작성 (with 사진) #19

---
+++ src/components/pages/products/ProductList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductListItem from '@/components/pages/products/ProductListItem';
+import { productDummyData } from '@/data/DummyData/ProductDummyData';
+export default function ProductListArticle() {
+  return (
+    <section>
+      <ul className=" grid grid-cols-2 justify-center p-6 gap-x-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+        {productDummyData.map((product) => (
+          <ProductListItem key={product.id} product={product} size={165} />
+        ))}
+      </ul>
+    </section>
+  );
+}
