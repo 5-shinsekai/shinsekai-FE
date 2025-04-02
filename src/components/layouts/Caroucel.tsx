86f7739adae4f2d32016277934e0eca8c799ac34
unknown <corosica1@gmail.com>
2025-04-02 17:21:37 +0900

    Fix : 불필요 요소 삭제

--- src/components/layouts/Caroucel.tsx
+++ src/components/layouts/Caroucel.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import DummyEventImage from '@/components/images/DummyEventImage.png';
 
