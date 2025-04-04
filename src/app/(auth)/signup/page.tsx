aa751ea7ea9af80ea7040ab9784a7bd4726c6f6c
unknown <corosica1@gmail.com>
2025-04-04 09:23:03 +0900

    Feat : 회원가입 form, server action 관련 테스트

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,12 @@
-'use server';
 import { tempService } from '@/actions/signup-service';
 import { Button } from '@/components/ui/button';
 import PolicyAgreementPage from '@/components/pages/auth/signup/PolicyAgreementPage';
-export default async function Signup() {
+import SignUpStep1 from '@/components/pages/auth/signup/SignUpStep1';
+export default function Signup() {
   return (
     <main>
       <form action={tempService}>
+        <SignUpStep1 />
         <PolicyAgreementPage />
         <Button type="submit" />
       </form>
