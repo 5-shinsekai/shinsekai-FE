fff30d62a80d036934c90b70a1898b6fd7f464fd
unknown <corosica1@gmail.com>
2025-04-04 17:00:05 +0900

    Feat : 로딩 중 아이콘 추가

--- src/app/(products)/products/page.tsx
+++ src/app/(products)/products/page.tsx
@@ -1,47 +1,12 @@
 import MenuTab from '@/components/layouts/MenuTabModule';
 import ProductSubCategory from '@/components/pages/products/ProductSubCategory';
-import { CategoryDataType } from '@/types/ProductDataTypes';
+import { CategoryData } from '@/data/DummyData/CategoryDummyData';
 import React from 'react';
 export default function page() {
-  const Data: CategoryDataType[] = [
-    {
-      code: 1,
-      name: '전체',
-    },
-    {
-      code: 2,
-      name: '텀블러/보온병',
-    },
-    {
-      code: 3,
-      name: '머그/컵',
-    },
-    {
-      code: 4,
-      name: '라이프스타일',
-    },
-    {
-      code: 5,
-      name: '티/커피용품',
-    },
-    {
-      code: 6,
-      name: '케이크',
-    },
-    {
-      code: 7,
-      name: '초콜릿/스낵',
-    },
-    {
-      code: 8,
-      name: '세트',
-    },
-  ];
-
   return (
     <div>
       <MenuTab
-        data={{ keyname: 'highCategory', data: Data }}
+        data={{ keyname: 'highCategory', data: CategoryData }}
         isDefault={true}
         isMultiple={false}
       />
--- src/data/DummyData/CategoryDummyData.tsx
+++ src/data/DummyData/CategoryDummyData.tsx
@@ -1,4 +1,5 @@
 import { CategoryType } from '@/types/MenuTypes';
+import { CategoryDataType } from '@/types/ProductDataTypes';
 export const eventData: CategoryType = {
   keyname: 'eventName',
   data: [
@@ -62,3 +63,38 @@ export const bestData: CategoryType = {
     },
   ],
 };
+
+export const CategoryData: CategoryDataType[] = [
+  {
+    code: 1,
+    name: '전체',
+  },
+  {
+    code: 2,
+    name: '텀블러/보온병',
+  },
+  {
+    code: 3,
+    name: '머그/컵',
+  },
+  {
+    code: 4,
+    name: '라이프스타일',
+  },
+  {
+    code: 5,
+    name: '티/커피용품',
+  },
+  {
+    code: 6,
+    name: '케이크',
+  },
+  {
+    code: 7,
+    name: '초콜릿/스낵',
+  },
+  {
+    code: 8,
+    name: '세트',
+  },
+];
