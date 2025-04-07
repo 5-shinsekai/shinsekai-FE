94f5780d9940ca280a5d20374cabf4c1ea6a9d5e
unknown <corosica1@gmail.com>
2025-04-07 11:26:51 +0900

    Fix : 빌드 에러항목 수정

--- src/app/(main)/best/page.tsx
+++ src/app/(main)/best/page.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import MenuTab from '@/components/layouts/MenuTabModule';
+import React, { Suspense } from 'react';
+import MenuTab from '@/components/layouts/MenuTab';
 import ProductList from '@/components/pages/products/ProductList';
 import { bestData } from '@/data/DummyData/CategoryDummyData';
 export default function page() {
   return (
     <main>
       <nav className=" sticky top-28 shadow z-10 bg-white ">
-        <MenuTab data={bestData} isDefault={true} isMultiple={false} />
+        <Suspense>
+          <MenuTab data={bestData} isDefault={true} isMultiple={false} />
+        </Suspense>
       </nav>
       <ProductList bestTag={true} />
     </main>
