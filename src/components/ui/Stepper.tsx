d96d5df9b4dec49f2d7c07c8b771f29d3dd492dd
unknown <corosica1@gmail.com>
2025-04-11 10:22:41 +0900

    Feat : Stepper 일부 수정

--- src/components/ui/Stepper.tsx
+++ src/components/ui/Stepper.tsx
@@ -9,12 +9,12 @@ export default function Stepper({ totalSteps }: { totalSteps: number }) {
   }
 
   return (
-    <div className="flex items-center justify-center w-full my-8">
-      <div className="flex items-center">
+    <div className="items-center w-full">
+      <div className="flex items-center justify-center">
         {Array.from({ length: totalSteps }, (_, index) => (
           <React.Fragment key={index}>
             <div
-              className={`w-8 h-8 rounded-full flex items-center justify-center border-2 transition-colors delay-300 duration-500 ease-in-out
+              className={`w-6 h-6 rounded-full flex items-center justify-center border-2 transition-colors delay-300 duration-500 ease-in-out
                 ${
                   index + 1 <= steps
                     ? 'bg-black text-white border-black'
@@ -24,9 +24,8 @@ export default function Stepper({ totalSteps }: { totalSteps: number }) {
               {index + 1}
             </div>
 
-            {/* Connector Line */}
             {index < totalSteps - 1 && (
-              <div className="relative w-10 mx-1">
+              <div className="relative w-8">
                 <div className="absolute top-1/2 w-full h-0.5 bg-gray-300 -translate-y-1/2"></div>
                 <div
                   className="absolute top-1/2 h-0.5 bg-black -translate-y-1/2 origin-left transition-all duration-300 ease-out"
