92a5ac87571288e40595333430518a8724993874
unknown <corosica1@gmail.com>
2025-04-19 16:37:57 +0900

    Fix : Next-auth 로그인 redirection 관련 문제 해결

--- src/components/pages/auth/LogInSection.tsx
+++ src/components/pages/auth/LogInSection.tsx
@@ -1,26 +1,49 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import LogInForm from './LogInInForm';
 import AuthMenu from './AuthMenu';
 import ButtonWrapper from '@/components/ui/wrapper/ButtonWrapper';
 import { signIn } from 'next-auth/react';
 import { Button } from '@/components/ui/Button';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function LogInSection() {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const searchParams = useSearchParams();
   const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const loginId = formData.get('loginId') as string;
     const password = formData.get('password') as string;
-    signIn('credentials', {
-      loginId,
-      password,
-      redirect: true,
-    });
+
+    try {
+      const res = await signIn('credentials', {
+        loginId,
+        password,
+        redirect: false,
+      });
+
+      if (res?.error) {
+        console.log(res);
+        setError('아이디 또는 비밀번호가 일치하지 않습니다.');
+        return;
+      }
+
+      if (res?.ok) {
+        const callbackUrl = searchParams.get('callbackUrl');
+        console.log(callbackUrl);
+        router.push(callbackUrl || '/');
+      }
+    } catch (error) {
+      setError('로그인 중 오류가 발생했습니다.');
+    }
   };
+
   return (
     <form onSubmit={handleSignIn} className="h-full">
       <LogInForm />
+      {error && <div className="text-red-500 text-sm mb-4">{error}</div>}
       <AuthMenu />
       <ButtonWrapper>
         <Button
