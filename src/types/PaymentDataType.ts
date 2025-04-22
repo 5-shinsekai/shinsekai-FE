8d32a7ad00461425244013b97bc4cb1a9dc78245
yejiis <yeji020823@gmail.com>
2025-04-22 18:21:49 +0900

    feat: 결제 페이지 내 장바구니 체크 상품 정보 조회 기능 추가

--- src/types/PaymentDataType.ts
+++ src/types/PaymentDataType.ts
@@ -39,3 +39,64 @@ export interface StarbuckscardChargeOptionType {
   option: string;
   amount: number;
 }
+
+export interface CartOrderItemInfoType {
+  cartUuid: string;
+  productCode: string;
+  productOptionListId: number;
+  quantity: number;
+  checked: boolean;
+  engravingMessage: string;
+}
+
+export interface CartCheckedDataType {
+  ordinaryProducts: Partial<CartOrderItemInfoType>[];
+  frozenProducts: Partial<CartOrderItemInfoType>[];
+}
+
+export interface ProductDataType {
+  productCode: string;
+  productName: string;
+  productPrice: number;
+  productSummary: string;
+  contentImages: string;
+  thumbnailUrl: string;
+  userPurchaseLimit: number;
+  discountRate: number;
+  productOptionIds: [number];
+  frozen: boolean;
+  new: boolean;
+  best: boolean;
+  engraving: boolean;
+}
+
+export interface ShowOrderProductDataType {
+  productCode: string;
+  productName: string;
+  productPrice: number;
+  discountRate: number;
+  productOptionListId: number[];
+  quantity: number;
+  thumbnailUrl: string;
+}
+
+export interface ProductOptionDataType {
+  id: number;
+  productCode: string;
+  sizeId: number;
+  colorId: number;
+  optionStatus: string;
+  optionPrice: number;
+  stockCount: number;
+  minStockCount: number;
+}
+
+export interface ProductOutlineDataType {
+  productCode: string;
+  productName: string;
+  productPrice: number;
+  thumbnailUrl: string;
+  discountRate: number;
+  new: boolean;
+  best: boolean;
+}
